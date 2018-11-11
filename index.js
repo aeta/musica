8327b3c8bd2408557e8ebb0769c4eecf0b98b6f0
Alan Chu <ualch9@gmail.com>
2018-11-10 16:45:59 -0800

    initial

---
+++ index.js
@@ -0,0 +1,4 @@
+var fileInputElement = document.getElementById("file_input")
+fileInputElement.onchange = function(e) {
+    console.log(fileInputElement.value)
+}
---
+++ music.js
@@ -0,0 +1,20 @@
+var bleepAtFrequency = function(freq) {
+    o = context.createOscillator()
+    o.type = "square"
+    o.frequency.value = freq
+    o.start(0);
+    o.connect(context.destination)
+    o.stop(context.currentTime + 0.1)
+  }
+  
+  var play = function(r, g, b) {
+    bleepAtFrequency(findAverage(r, g, b))
+  }
+  
+  var findAverage = function (r, g, b) {
+    return (findFreq(r) + findFreq(g) + findFreq(b)) / 3
+  }
+  
+  var findFreq = function(value) {
+    return 200 * (2 ** (value / 127))
+  }
